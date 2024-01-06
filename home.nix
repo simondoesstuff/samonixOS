9e7851139e09ea04ac7fb69244a32e75b25a0a27
masoniis <bott.m@comcast.net>
2024-01-06 09:57:49 -0700

    added wezterm config file

--- home.nix
+++ home.nix
@@ -1,65 +1,61 @@
 { config, pkgs, ... }:
 
-let
-	commonPackages = [ # Packages that are common to all platforms
-		# pkgs.cowsay
-		pkgs.neovim
-	];
+{
+	home = {
+		#INFO: The user account and path that Home Manager will manage
+		username = "mason";
+		homeDirectory = "/Users/mason";
 
-	platformPackages = pkgs.lib.optionals pkgs.hostPlatform.isDarwin [
-    (pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Darwin!"'')
-	] ++ pkgs.lib.optionals pkgs.hostPlatform.isWindows [
-   (pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Windows!"'')
-	] ++ pkgs.lib.optionals pkgs.hostPlatform.isLinux [
-   (pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Linux!"'')
-	];
-in
-	{
-		home = {
-			#INFO: The user account and path that Home Manager will manage
-			username = "mason";
-			homeDirectory = "/Users/mason";
+		#WARNING: You should not change this value, even if you update Home Manager. Check release notes if desired to change.
+		stateVersion = "23.11";
 
-			#WARNING: You should not change this value, even if you update Home Manager. Check release notes if desired to change.
-			stateVersion = "23.11";
+		packages = [
+			pkgs.neovim
+			pkgs.starship # Cool shell prompt
+		] ++ pkgs.lib.optionals pkgs.hostPlatform.isDarwin [
+  		(pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Darwin!"'')
+			pkgs.wezterm # Mac linux terminal
+		] ++ pkgs.lib.optionals pkgs.hostPlatform.isWindows [
+		 (pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Windows!"'')
+		] ++ pkgs.lib.optionals pkgs.hostPlatform.isLinux [
+		 (pkgs.writeShellScriptBin "hello" ''echo "Hello, ${config.home.username}, from nix Linux!"'')
+		];
+		# Home Manager is pretty good at managing dotfiles. The primary way to manage
+		# plain files is through 'home.file'.
+		file = {
 
-			packages = commonPackages ++ platformPackages; # Combine the common and platform-specific packages to install
+			# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+			# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+			# # symlink to the Nix store copy.
+			# ".screenrc".source = dotfiles/screenrc;
 
-			# Home Manager is pretty good at managing dotfiles. The primary way to manage
-			# plain files is through 'home.file'.
-			file = {
-
-				# # Building this configuration will create a copy of 'dotfiles/screenrc' in
-				# # the Nix store. Activating the configuration will then make '~/.screenrc' a
-				# # symlink to the Nix store copy.
-				# ".screenrc".source = dotfiles/screenrc;
-
-				# # You can also set the file content immediately.
-				# ".gradle/gradle.properties".text = ''
-				#   org.gradle.console=verbose
-				#   org.gradle.daemon.idletimeout=3600000
-				# '';
-			};
-
-			# Manage environment (shell) variables
-			sessionVariables = {
-				EDITOR = "neovim";
-			};
+			# # You can also set the file content immediately.
+			# ".gradle/gradle.properties".text = ''
+			#   org.gradle.console=verbose
+			#   org.gradle.daemon.idletimeout=3600000
+			# '';
 		};
 
-		nixpkgs.config.allowUnfree = true; # Allow unfree licensed packages, like discord
-
-
-		nix = { # Configure the Nix package manager
-			package = pkgs.nix;
-			settings.experimental-features = [ "nix-command" ];
+		# Manage environment (shell) variables
+		sessionVariables = {
+			EDITOR = "neovim";
 		};
+	};
 
-		xdg.configFile.nvim = {
-			source = ./config/nvim;
-			recursive = true;
-		};
+	imports = [ ./programs/wezterm.nix ]; # Import the configuration for wezterm
 
-		# Let Home Manager install and manage itself.
-		programs.home-manager.enable = true;
-	}
+	nixpkgs.config.allowUnfree = true; # Allow unfree licensed packages, like discord
+
+	nix = { # Configure the Nix package manager
+		package = pkgs.nix;
+		settings.experimental-features = [ "nix-command" ];
+	};
+
+	xdg.configFile.nvim = {
+		source = ./config/neovim;
+		recursive = true;
+	};
+
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
+}
---
+++ programs/wezterm.nix
@@ -0,0 +1,148 @@
+{ config, pkgs, ... }:
+
+{
+  programs.wezterm = {
+		enable = true;
+		extraConfig = "
+				local wezterm = require 'wezterm'
+
+				-- This table will hold the configuration.
+				local config = {}
+
+				-- In newer versions of wezterm, use the config_builder which will
+				-- help provide clearer error messages
+				if wezterm.config_builder then
+					config = wezterm.config_builder()
+				end
+
+				-- config.default_cursor_style = 'SteadyBar' -- Set default cursor to not be a block
+
+				--config.default_gui_startup_args = {}
+
+				-- config.ssh_domains = {
+				--   {
+				--     -- This name identifies the domain
+				--     name = 'my.server',
+				--     -- The hostname or address to connect to. Will be used to match settings
+				--     -- from your ssh config file
+				--     remote_address = '192.168.1.1',
+				-- 		multiplexing = 'None',
+				--     -- The username to use on the remote host
+				--     username = 'mason',
+				--   },
+				-- }
+				--
+				-- config.default_domain = 'my.server'
+
+				-- This is where you actually apply your config choices
+
+				-- For example, changing the color scheme:
+				config.color_scheme = 'tokyonight'
+				config.default_cursor_style = 'BlinkingBar'
+				-- Window transparency // opacity
+				-- config.window_background_opacity = 0.92
+
+				config.hide_tab_bar_if_only_one_tab = true
+
+				-- Don't prompt on close
+				config.window_close_confirmation = 'NeverPrompt'
+
+				-- Removing the weird window padding from terminal
+				config.window_padding = {
+					left = 0,
+					right = 0,
+					top = 0,
+					bottom = 0,
+				}
+				config.animation_fps = 32
+				config.font = wezterm.font 'Hack Nerd Font' -- Nerd font, glyps can be found on website
+
+				-- Config for the window frame
+				config.window_frame = {
+					-- font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+					-- The size of the font in the tab bar.
+					-- Default to 10. on Windows but 12.0 on other systems
+					font_size = 12.0,
+
+					-- The overall background color of the tab bar when
+					-- the window is focused
+					active_titlebar_bg = '#333333',
+
+					-- The overall background color of the tab bar when
+					-- the window is not focused
+					inactive_titlebar_bg = '#333333',
+				}
+
+				config.colors = {
+					tab_bar = {
+						-- The active tab is the one that has focus in the window
+						active_tab = {
+							-- The color of the background area for the tab
+							bg_color = '#1A1B26',
+							-- The color of the text for the tab
+							fg_color = '#c0c0c0',
+
+							intensity = 'Normal',
+
+							underline = 'None',
+
+							-- Specify whether you want the text to be italic (true) or not (false)
+							-- for this tab.  The default is false.
+							italic = false,
+
+							-- Specify whether you want the text to be rendered with strikethrough (true)
+							-- or not for this tab.  The default is false.
+							strikethrough = false,
+						},
+
+					-- The divider between tabs
+					inactive_tab_edge = '#333333',
+
+						-- Inactive tabs are the tabs that do not have focus
+						inactive_tab = {
+							bg_color = '#333333',
+							fg_color = '#808080',
+
+							-- The same options that were listed under the `active_tab` section above
+							-- can also be used for `inactive_tab`.
+						},
+
+						-- You can configure some alternate styling when the mouse pointer
+						-- moves over inactive tabs
+						inactive_tab_hover = {
+							bg_color = '#3b3052',
+							fg_color = '#909090',
+							italic = true,
+
+							-- The same options that were listed under the `active_tab` section above
+							-- can also be used for `inactive_tab_hover`.
+						},
+
+						-- The new tab button that let you create new tabs
+						new_tab = {
+							bg_color = '#333333',
+							fg_color = '#808080',
+
+							-- The same options that were listed under the `active_tab` section above
+							-- can also be used for `new_tab`.
+						},
+
+						-- You can configure some alternate styling when the mouse pointer
+						-- moves over the new tab button
+						new_tab_hover = {
+							bg_color = '#3b3052',
+							fg_color = '#909090',
+							italic = true,
+
+							-- The same options that were listed under the `active_tab` section above
+							-- can also be used for `new_tab_hover`.
+						},
+					},
+				}
+
+				-- and finally, return the configuration to wezterm
+				return config
+		";
+	};
+}
