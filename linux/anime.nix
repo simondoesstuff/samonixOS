aa45c83c9a94144bef3c3023225b0da11b67f90d
masoniis <bott.m@comcast.net>
2024-03-12 08:43:09 -0600

    linux jerry support

--- linux/anime.nix
+++ linux/anime.nix
@@ -2,7 +2,15 @@
 
 #INFO: Version 4.0.1 of Anime4k
 let 
-	fetchFromGitHub = pkgs.fetchFromGitHub;
+	jerry = pkgs.stdenv.mkDerivation {
+		name = "jerry 1.9.9";
+		src = pkgs.fetchurl {
+			url = "https://github.com/justchokingaround/jerry/raw/main/jerry.sh";
+			sha256 = "1jcc0cakrdxarqps6r2v03xvdqd3hmj63qw0fbzzgda0ydinappq";
+		};
+		dontUnpack = true;
+		installPhase = "install -D $src $out/bin/jerry";
+	};
 in 
 {
 # Note, to use with IINA must edit settings to point to mpv configuration
@@ -16,14 +24,6 @@ in
 	# 	recursive = true;
 	# };
 
-	home.file."/usr/local/bin/jerry" = {
-		source = pkgs.fetchurl {
-			url = "https://github.com/justchokingaround/jerry/raw/main/jerry.sh";
-			sha256 = "1jcc0cakrdxarqps6r2v03xvdqd3hmj63qw0fbzzgda0ydinappq"; # nix-preferch-url hash
-		};
-		executable = true;
-	};
-
 	home.packages = with pkgs; [
 		# LOBSTER
 		mpv # Video player for cli
@@ -33,5 +33,7 @@ in
 		socat # Vid positions
 		ueberzugpp # images in terminal
 		gnupatch
+		jerry
 	];
 }
+
