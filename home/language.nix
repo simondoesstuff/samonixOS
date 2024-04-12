307b5fe0abeef25817e696afbb9051ef558f92d2
masoniis <bott.m@comcast.net>
2024-04-11 18:26:43 -0600

    git conflict for nvim, glsl cleanups

--- home/language.nix
+++ home/language.nix
@@ -1,15 +1,12 @@
 { pkgs, ... }:
 
 let
-	#https://github.com/nolanderc/glsl_analyzer/releases/download/v1.4.4/aarch64-macos.zip
-  version = "1.4.4";
-  url = "https://github.com/nolanderc/glsl_analyzer/releases/download/v${version}/aarch64-macos.zip";
-
+	version = "1.4.4";
 	glslanalyzer = pkgs.stdenv.mkDerivation {
 		pname = "glslanalyzer";
-		version = "1.4.4";
+		version = version;
 
-		src = pkgs.fetchzip {
+		 src = pkgs.fetchzip {
 			url = "https://github.com/nolanderc/glsl_analyzer/releases/download/v${version}/aarch64-macos.zip";
 			sha256 = "1p1crnfbfa9jl96p8m44mpa7fw2n0bdabp67vhmy74a3j26dnl3f";
 		};
