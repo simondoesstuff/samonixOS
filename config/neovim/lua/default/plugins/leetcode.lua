ae3fc59e9c888504e7b3fa44934ed6e973408598
masoniis <bott.m@comcast.net>
2024-04-23 16:06:34 -0600

    leetcode changes

--- config/neovim/lua/default/plugins/leetcode.lua
+++ config/neovim/lua/default/plugins/leetcode.lua
@@ -14,17 +14,23 @@ return {
 	},
 	opts = {
 		lang = "python3",
-		hooks = {
-		}
 	},
 	config = function()
 		local leetcode = require("leetcode");
-		leetcode.setup({})
+		leetcode.setup({
+			lang = "python3",
+		})
+
+		vim.cmd("Copilot disable")
 
 		vim.keymap.set("n", "<leader>L", " ", { desc = "Leet" })
 		vim.keymap.set("n", "<leader>Ll", "<cmd>Leet lang<cr>", { desc = "Leet lang" })
 		vim.keymap.set("n", "<leader>Lc", "<cmd>Leet console<cr>", { desc = "Leet console" })
-		vim.keymap.set("n", "<leader>Lr", "<cmd>Leet test<cr>", { desc = "Leet test" })
+		vim.keymap.set("n", "<leader>Lt", "<cmd>Leet test<cr>", { desc = "Leet test" })
+		vim.keymap.set("n", "<leader>Ls", "<cmd>Leet submit<cr>", { desc = "Leet submit" })
 		vim.keymap.set("n", "<leader>Lr", "<cmd>Leet run<cr>", { desc = "Leet run" })
+		vim.keymap.set("n", "<leader>Li", "<cmd>Leet info<cr>", { desc = "Leet info" })
+		vim.keymap.set("n", "<leader>Ld", "<cmd>Leet desc<cr>", { desc = "Leet desc" })
+		vim.keymap.set("n", "<leader>Lp", "<cmd>Leet list<cr>", { desc = "Leet problem list" })
 	end
 }
