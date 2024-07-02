eee1702fd254e8dbc7cb986d457f83cea8267b36
masoniis <bott.m@comcast.net>
2024-07-01 21:57:14 -0600

    minor cleanups on toggleterm comments

--- config/neovim/lua/default/plugins/toggleterm.lua
+++ config/neovim/lua/default/plugins/toggleterm.lua
@@ -9,17 +9,14 @@ return {
 			cmd = "lazygit",
 			dir = "git_dir",
 			direction = "float",
-			-- function to run on opening the terminal
 			on_open = function(term)
 				vim.cmd("startinsert!")
 				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
 			end,
-			-- function to run on closing the terminal
-			on_close = function(term)
+			on_close = function()
 				vim.cmd("startinsert!")
 			end,
 		})
-		-- local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
 		function _Lazygit_Toggle()
 			lazygit:toggle()
