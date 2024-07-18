c6e7c011ada87000dad968f1588e1e5c46380b51
masoniis <bott.m@comcast.net>
2024-07-18 13:05:21 -0600

    default 100 volume for spotify-player

--- hosts/darwin/spotify.nix
+++ hosts/darwin/spotify.nix
@@ -9,10 +9,13 @@
 		enable = true;
 		settings = {
 			enable_notify = false;
+			device = {
+				volume = 100; # make initial volume 70 -> 100 so that laptop has full control
+			};
 		};
 		keymaps = [
 			{
-				command = "None"; # Disable q for toggleterm, so that we can leave terminal without closing player
+				command = "None"; # Disable q for nvim toggleterm, so that we can leave terminal without closing player
 				key_sequence = "q";
 			}
 		];
