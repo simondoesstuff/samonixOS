35b299118a29cb4d0551bf6ade7a1718b39f6820
masoniis <bott.m@comcast.net>
2024-08-10 10:08:49 -0600

    added cool code action menu to lang serv setup

--- config/neovim/lua/default/plugins/lsp/lspconfig.lua
+++ config/neovim/lua/default/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "folke/neoconf.nvim", "folke/neodev.nvim", "hrsh7th/cmp-nvim-lsp" },
+		dependencies = { "folke/neoconf.nvim", "folke/neodev.nvim", "hrsh7th/cmp-nvim-lsp", "aznhe21/actions-preview.nvim" },
 		config = function()
 			local lspconfig = require('lspconfig')
 
@@ -78,7 +78,9 @@ return {
 					vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { buffer = bufnr, desc = "see implementation" })
 					vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "type definition" })
 					vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { buffer = bufnr, desc = "rename symbol" })
-					vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { buffer = bufnr, desc = "code action" })
+					vim.keymap.set("n", "<leader>la", require("actions-preview").code_actions,
+						{ buffer = bufnr, desc = "code action menu" })
+
 					vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { buffer = bufnr, desc = "references" })
 					vim.keymap.set("n", "<leader>lf", function() vim.diagnostic.open_float() end,
 						{ buffer = bufnr, desc = "diagnostics float" })
@@ -88,10 +90,30 @@ return {
 		end,
 	},
 	{
+		"aznhe21/actions-preview.nvim", -- Preview menu for code actions
+		opts = {
+			telescope = require("telescope.themes").get_dropdown {
+				sorting_strategy = "ascending",
+				layout_strategy = "vertical",
+				winblend = 10,
+				layout_config = {
+					-- anchor = "CENTER",  -- vertically and horizontally
+					width = 0.8,
+					height = 0.9,
+					prompt_position = "top",
+					preview_cutoff = 25,
+					preview_height = function(_, _, max_lines)
+						return max_lines - 25
+					end,
+				},
+			},
+		},
+	},
+	{
 		"j-hui/fidget.nvim", -- Fidget spinner showing lsp processes
 		tag = "legacy",    -- To avoid breaking changes
 		config = function()
 			require("fidget").setup({})
 		end,
-	},
+	}
 }
