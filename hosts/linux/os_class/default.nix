b57fec8e59c90869e8ad20c234e20bb7f8669003
masoniis <bott.m@comcast.net>
2024-08-29 14:19:26 -0600

    removed most OS class imports

--- hosts/linux/os_class/default.nix
+++ hosts/linux/os_class/default.nix
@@ -1,11 +1,11 @@
 {
 	imports = [
-		../packages.nix
-		../zsh.nix
-		../../shared/nvim.nix
-		../../shared/starship.nix
-		../../shared/zoxide.nix
-		../../shared/eza.nix
 		../../shared/git.nix
+		../../shared/nvim.nix
+		# ../packages.nix
+		# ../zsh.nix
+		# ../../shared/starship.nix
+		# ../../shared/zoxide.nix
+		# ../../shared/eza.nix
 	]; 
 }
