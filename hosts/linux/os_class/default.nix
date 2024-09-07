76c9b4d41e6a018dd00d1e15ac6a8175147348c4
masoniis <bott.m@comcast.net>
2024-09-07 10:16:14 -0600

    zoxide add to os

--- hosts/linux/os_class/default.nix
+++ hosts/linux/os_class/default.nix
@@ -2,10 +2,6 @@
 	imports = [
 		../../shared/git.nix
 		../../shared/nvim.nix
-		# ../packages.nix
-		# ../zsh.nix
-		# ../../shared/starship.nix
-		# ../../shared/zoxide.nix
-		# ../../shared/eza.nix
+		../../shared/zoxide.nix
 	]; 
 }
