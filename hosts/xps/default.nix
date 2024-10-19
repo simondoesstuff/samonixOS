358e7af5a00f6bb7e1e03ee65a96552a74f108ea
masoniis <bott.m@comcast.net>
2024-10-19 10:49:05 -0600

    basic xps nixos conf

---
+++ hosts/xps/default.nix
@@ -0,0 +1,26 @@
+{inputs, ...}:
+let
+  inherit (inputs) nixpkgs nixos-wsl home-manager masonpkgs;
+in
+  nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    # specialArgs = {inherit nixos-wsl;};
+    modules = [
+      nixos-wsl.nixosModules.wsl # nixos-wsl modules
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.mason = {...}: {
+          imports = [./temphome.nix ../../modules/linux/default.nix];
+        };
+
+        home-manager.extraSpecialArgs = {
+          username = "mason";
+          root = ../..;
+					masonpkgs = masonpkgs;
+        };
+      }
+    ];
+  }
