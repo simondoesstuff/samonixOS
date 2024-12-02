e3ef76c54cb3a4d3c73c9e55354b4643c4afab24
masoniis <bott.m@comcast.net>
2024-12-02 15:42:53 -0700

    flake update, disable openASAR not worth the hassle

--- modules/common/personal/gaming.nix
+++ modules/common/personal/gaming.nix
@@ -10,8 +10,9 @@
     home.packages = [
       #INFO: On mac, since this modifies signed discord, it now has to be allowed in security settings.
       # ALso, see https://github.com/NixOS/nixpkgs/issues/208749 for errors with openasar "module not found"
+      # TLDR: Open regular discord once to get openASAR to work, and then enable openASAR
       (pkgs.discord.override {
-        withOpenASAR = true;
+        # withOpenASAR = true;
         withVencord = true;
       })
     ];
