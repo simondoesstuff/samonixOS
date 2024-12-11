d7db53e44063c3d2fb187e5de515c6b31ea29fd7
masoniis <bott.m@comcast.net>
2024-12-11 21:40:04 +0000

    ssh key for windows 3080

--- hosts/wslonix/configuration.nix
+++ hosts/wslonix/configuration.nix
@@ -23,6 +23,7 @@
 		openssh.authorizedKeys = {
 			keys = [
 				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZblT7Q/WxYTQnb3WL9lJMclp1DeQeYzdBKOBPAX0bD" # mbp14
+				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqYhMkfTYA7biVs4xp0OxhcV0Zk4yxvMTLn7u6S0PWc" # windows 3080pc
 			];
 		};
   };
