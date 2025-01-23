2dbdca5a91975a1b36b15ad2e6ce58043c15d1f7
masoniis <bott.m@comcast.net>
2025-01-23 13:34:10 -0700

    shell formatting

--- modules/common/dev/termshell.nix
+++ modules/common/dev/termshell.nix
@@ -3,61 +3,60 @@
   config,
   ...
 }: {
-  # INFO: Terminal stuff
-  programs.wezterm.enable = true;
-
-  # Source wezterm dotfile directly
-  xdg.configFile.wezterm = {
-    source = root + /dotfiles/wezterm;
-    recursive = true;
-  };
-  xdg.configFile.ghostty = {
-    source = root + /dotfiles/ghostty;
-    recursive = true;
-  };
-
-  # INFO: Shell stuff
-  programs.zsh = {
-    enable = true;
-    syntaxHighlighting.enable = true;
-    autosuggestion.enable = true;
-    enableCompletion = true;
-    # Added to (end) of .zprofile
-    profileExtra =
-      if config.isDarwin
-      # 1st export: Add docker to path, docker desktop isn't on nix I think?
-      # 2nd export: Add flutter to path, manually install because using nix on mac wouldn't work
-      # 3rd export: Setting chrome executable for flutter to read from to launch web apps
-      then ''
-        export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin/"
-        export PATH=$PATH:$HOME/dev/sdk/flutter/bin:$PATH
-        export CHROME_EXECUTABLE="/Applications/Microsoft Edge Dev.app/Contents/MacOS/Microsoft Edge Dev"
-      ''
-      else "";
-    # Added to (end) of .zshrc, init zoxide as per docs
-    initExtra = "
+  # INFO: Programs
+  programs = {
+    wezterm.enable = true; # terminal emulator
+    starship.enable = true; # shell prompts
+    # better cd command
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    # better ls command
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      # enableAliases = true; # Default aliases
+    };
+    # shell
+    zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      # Added to (end) of .zprofile
+      profileExtra =
+        if config.isDarwin
+        # 1st export: Add docker to path, docker desktop isn't on nix I think?
+        # 2nd export: Add flutter to path, manually install because using nix on mac wouldn't work
+        # 3rd export: Setting chrome executable for flutter to read from to launch web apps
+        then ''
+          export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin/"
+          export PATH=$PATH:$HOME/dev/sdk/flutter/bin:$PATH
+          export CHROME_EXECUTABLE="/Applications/Microsoft Edge Dev.app/Contents/MacOS/Microsoft Edge Dev"
+        ''
+        else "";
+      # Added to (end) of .zshrc, init zoxide as per docs
+      initExtra = "
 			eval \"$(zoxide init zsh --cmd cd)\"
 		";
-    completionInit = "
+      completionInit = "
 			bindkey '^ ' autosuggest-accept
 		";
+    };
   };
 
-  # Shell prompt
-  programs.starship = {
-    enable = true;
-  };
+  # home.packages = with pkgs; [ghostty]; broken on mac currently
 
-  # Zoxide is a better form of 'cd' (chang dir)
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  # Better ls
-  programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
-    # enableAliases = true; # Default aliases
+  # INFO: Source dotfiles directly
+  xdg.configFile = {
+    wezterm = {
+      source = root + /dotfiles/wezterm;
+      recursive = true;
+    };
+    ghostty = {
+      source = root + /dotfiles/ghostty;
+      recursive = true;
+    };
   };
 }
