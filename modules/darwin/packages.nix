9202d9b8ce022842a1403a14198143c90dc1e0f3
masoniis <bott.m@comcast.net>
2025-01-23 13:14:52 -0700

    add zotero to darwin

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
     ollama # run local ml models
     iina # sleek video player (MPV replacement)
+    zotero # reference manager
 
     # TODO: Migrate these to nix
     # raycast
