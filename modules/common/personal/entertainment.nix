2c6bc30685dcea20fd90cdaac71a1e65b89ac75f
masoniis <bott.m@comcast.net>
2025-02-22 23:46:52 -0700

    merged spotify into ent

--- modules/common/personal/entertainment.nix
+++ modules/common/personal/entertainment.nix
@@ -48,11 +48,29 @@
           then true
           else false;
       })
+      pkgs.spotifyd
     ];
+
+    programs.spotify-player = {
+      enable = true;
+      settings = {
+        enable_notify = false;
+        device = {
+          volume = 100; # make initial volume 70 -> 100 so that laptop has full control
+        };
+      };
+      keymaps = [
+        {
+          command = "None"; # Disable q for nvim toggleterm, so that we can leave terminal without closing player
+          key_sequence = "q";
+        }
+      ];
+    };
 
     home.shellAliases = {
       jerry = "command jerry --dub";
       jerrysub = "command jerry";
+      spt = "spotify_player";
     };
   };
 }
