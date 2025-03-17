607e1794f5200bcb075aa49e99451102c421c53c
Simon Walker <simon@simonwalker.tech>
2025-03-17 13:20:45 -0600

    faster window resize

--- modules/common/dev/nixvim/config/nav/smartsplits.nix
+++ modules/common/dev/nixvim/config/nav/smartsplits.nix
@@ -20,9 +20,8 @@
       ''
         require("smart-splits").setup({
         	cursor_follows_swapped_bufs = true, -- When swapping, follow with the swap window (default false)
-        	default_amount = 1,              -- set default resize interval to 1 instead of 3
+        	default_amount = 6,
         })
-        vim.keymap.set("n", "<leader><leader>", " ", { desc = "swap splits" })
       '';
 
     keymaps = [
