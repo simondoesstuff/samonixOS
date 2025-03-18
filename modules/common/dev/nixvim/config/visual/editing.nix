63f21c9fd14c7ec652ede8299f0c6b36a6b70dc2
Simon Walker <simon@simonwalker.tech>
2025-03-18 08:50:27 -0600

    intellitab.nvim

--- modules/common/dev/nixvim/config/visual/editing.nix
+++ modules/common/dev/nixvim/config/visual/editing.nix
@@ -1,5 +1,5 @@
 # related to the visual editting experience!
-{...}: {
+{pkgs, ...}: {
   programs.nixvim = {
     plugins = {
       indent-blankline = {
@@ -13,6 +13,19 @@
       };
       illuminate.enable = true;
       todo-comments.enable = true;
+      intellitab = {
+        enable = true;
+        # TODO: remove custom intellitab package once upstream accepts pr
+        package = pkgs.vimUtils.buildVimPlugin {
+          name = "intellitab";
+          src = pkgs.fetchFromGitHub {
+            owner = "simondoesstuff";
+            repo = "intellitab.nvim";
+            rev = "fad06f80b44186767eba348283c3b88827a969b1";
+            sha256 = "sha256-ShMECcqFQS607ZWRKHEh1cSLT/XChx5JpNOHSP3h/7I=";
+          };
+        };
+      };
     };
   };
 }
