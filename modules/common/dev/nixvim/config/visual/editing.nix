a17e43b283405c80ecf1a05b668f03083dc4b53c
Simon Walker <simon@simonwalker.tech>
2025-03-18 08:21:38 -0600

    illuminate.nvim

--- modules/common/dev/nixvim/config/visual/editing.nix
+++ modules/common/dev/nixvim/config/visual/editing.nix
@@ -1,14 +1,17 @@
 # related to the visual editting experience!
 {...}: {
   programs.nixvim = {
-    plugins.indent-blankline = {
-      enable = true;
-      settings = {
-        scope = {
-          show_end = false;
-          show_start = false;
+    plugins = {
+      indent-blankline = {
+        enable = true;
+        settings = {
+          scope = {
+            show_end = false;
+            show_start = false;
+          };
         };
       };
+      illuminate.enable = true;
       todo-comments.enable = true;
     };
   };
