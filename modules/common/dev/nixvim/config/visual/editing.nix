ced98aeaa67b9f323ea7662ff17e51463cd35aaf
Simon Walker <simon@simonwalker.tech>
2025-03-19 10:07:05 -0600

    snacks.nvim indent guides

--- modules/common/dev/nixvim/config/visual/editing.nix
+++ modules/common/dev/nixvim/config/visual/editing.nix
@@ -2,15 +2,6 @@
 {pkgs, ...}: {
   programs.nixvim = {
     plugins = {
-      indent-blankline = {
-        enable = true;
-        settings = {
-          scope = {
-            show_end = false;
-            show_start = false;
-          };
-        };
-      };
       illuminate.enable = true;
       todo-comments.enable = true;
       intellitab = {
