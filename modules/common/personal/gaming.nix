4056ecf9b3766fc676acf06ae9cbe7efea9400da
mason <58895787+masoniis@users.noreply.github.com>
2025-05-01 12:25:19 -0600

    disabled C-l in terms for window nav so completion prevails

--- modules/common/personal/gaming.nix
+++ modules/common/personal/gaming.nix
@@ -11,11 +11,12 @@
     home.packages = [
       #INFO: On mac, since this modifies signed discord, it now has to be allowed in security settings.
       # ALso, see https://github.com/NixOS/nixpkgs/issues/208749 for errors with openasar "module not found"
-      # TLDR: Open regular discord once to get openASAR to work, and then enable openASAR
+      # TLDR: Open regular discord once to get openASAR to work, and then enable openASAR if you desire it
       (pkgs.discord.override {
         # withOpenASAR = true;
         withVencord = true;
       })
+      pkgs.signal-desktop
     ];
   };
 }
