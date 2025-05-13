86e59e496cd9b7bb99dcdd54d911376abbcbb080
Simon Walker <simon@simonwalker.tech>
2025-05-12 20:10:09 -0600

    disable py globals, uv only

--- modules/common/dev/languages/python.nix
+++ modules/common/dev/languages/python.nix
@@ -8,21 +8,19 @@
   config = lib.mkIf config.language.python.enable {
     home.packages = with pkgs; [
       uv
-      (python3.withPackages (
-        ps: with ps; [
-          black # formatter
-          isort # import sorter
-          pyright # lang server for type checking
-          python-lsp-server # other lang server
-          matplotlib
-          numpy
-          bandit
-        ]
-      ))
+      # (python3.withPackages (
+      #   ps: with ps; [
+      #     black # formatter
+      #     isort # import sorter
+      #     matplotlib
+      #     numpy
+      #     bandit
+      #   ]
+      # ))
     ];
 
-    programs.poetry = {
-      enable = true;
-    };
+    # programs.poetry = {
+    #   enable = true;
+    # };
   };
 }
