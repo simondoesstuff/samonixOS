da3f421227eb27ebceb93580d00f9c06ff74578b
Simon Walker <simon@simonwalker.tech>
2025-06-05 19:02:05 +0000

    sshd

--- hosts/wslOnix/configuration.nix
+++ hosts/wslOnix/configuration.nix
@@ -24,12 +24,6 @@ in
   users.users.simon = {
     ignoreShellProgramCheck = true; # shell is defined via home-manager
     shell = pkgs.zsh;
-    openssh.authorizedKeys = {
-      keys = [
-        # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZblT7Q/WxYTQnb3WL9lJMclp1DeQeYzdBKOBPAX0bD" # mbp14
-        # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqYhMkfTYA7biVs4xp0OxhcV0Zk4yxvMTLn7u6S0PWc" # windows 3080pc
-      ];
-    };
     home = "/mnt/c/home"; # let ~/ be easily accessible for both systems
   };
 
@@ -64,22 +58,28 @@ in
   };
 
   # INFO: Networking stuff
-  # networking = {
-  #   hostName = hostname;
-  #   networkmanager.enable = true;
-  #   nameservers = [
-  #     "8.8.8.8"
-  #     "8.8.4.4"
-  #   ];
-  # };
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = true;
+    nameservers = [
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
+  };
 
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
+    ports = [ 22565 ];
     settings = {
       PasswordAuthentication = false;
     };
   };
+
+  users.users.simon.openssh.authorizedKeys.keys = [
+    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZblT7Q/WxYTQnb3WL9lJMclp1DeQeYzdBKOBPAX0bD" # mbp14 (mason)
+    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqYhMkfTYA7biVs4xp0OxhcV0Zk4yxvMTLn7u6S0PWc" # windows 3080pc (mason)
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSzAG3Xgy9Sf0Ype3TolDXEtaBCtiUMyexxT1p67WbPOxR1/4qj4d3HgNezm+9Q+SSI4oYUofU552zdp5kUBdi19A3B5NWNEBmka/AGPEBer9UmbghgdtjzB+Fiqq6iBJAO4jnfaW29M7tlMMX6BgQHjgn5F3qblY4QKYD+d2e2LIJcbvTTIf91bFNhF4UUzfOMcXVIEmvrMhOB5ytKakoMrhFzFPPtQC7/R6fKd+Ua5yth9swp8Yp4OPbYXhm9mEhBtHyePUZQBvo7eq5if7VurCJ945PInnEgdR87YNpN6SvXLmSxmFtwABrCARzp0rYSmd71SX1PXHBvGdKp7y8pfsSv349j7+/n+qzF6dEPn7V0tvZa91+HHxxNukmhGS3UhPPX+481BvHi4HqjEA0Aj492gr/AfUIfcYkUkuztTvzbIumtOeTzkahT/sVSBVjTDpkkp4eP8ziKJjybO1yVeL+HnxF4Vr+4BOUnOecaJ52zeSOhG8MApFllnJGD9U= simon@EuclidMac.local" # euclidmac
+  ];
 
   # INFO: Other
   services.openvscode-server = {
