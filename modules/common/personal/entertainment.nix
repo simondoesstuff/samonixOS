7f269e722dfc89b6960c35b99d2f90dc83455e3d
mason <58895787+masoniis@users.noreply.github.com>
2025-06-15 15:51:06 -0600

    even even more mpv tweaks

--- modules/common/personal/entertainment.nix
+++ modules/common/personal/entertainment.nix
@@ -73,6 +73,14 @@ in
         source = "${pkgs.mpvScripts.modernz}/share/fonts";
         recursive = true;
       };
+      # Example loading external script
+      # "mpv/scripts/progressbar.lua" = {
+      #   # cooler progress bar
+      #   source = pkgs.fetchurl {
+      #     url = "https://github.com/torque/mpv-progressbar/releases/download/2025.02.09.0/progressbar-2025.02.06.0.lua";
+      #     sha256 = "sha256-klEk2UU4SG+MorSaA3ty5ydAVONIMeeqEPSQ9vElJHY=";
+      #   };
+      # };
     };
 
     programs.mpv = {
@@ -82,9 +90,10 @@ in
         modernz # epic ui
         smartskip # skip openings of shows
         mpvacious
+        thumbfast # thumbnailer preview when hovering progress bar
         autoload # auto load playlist entries for sequential files
         sponsorblock
-        eisa01.smart-copy-paste-2
+        eisa01.smart-copy-paste-2 # paste files into mpv player (subtitles, video)
       ];
 
       scriptOpts = {
@@ -96,6 +105,12 @@ in
           window_top_bar = false;
           jump_buttons = false; # dont show "jump 10 seconds" buttons
           nibbles_style = "single-bar";
+          fade_alpha = 185; # ensures covering the default OSD bar when paused
+          fade_blur_strength = 100;
+          thumbnail_border_color = "#000000";
+          thumbnail_border_outline = "#000000";
+          thumbnail_border_radius = 0;
+          thumbnail_border = 0;
         };
         SmartSkip = {
           silence_skip_to_end = "pause"; # pause if silent skip reaches the end
