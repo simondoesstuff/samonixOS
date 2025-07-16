4df2aa450d1dd53eb304c0bb15d5b1ab8bd756b8
Simon Walker <simon@simonwalker.tech>
2025-07-15 19:02:42 -0600

    wsl: new timezone

--- hosts/wslOnix/configuration.nix
+++ hosts/wslOnix/configuration.nix
@@ -10,6 +10,7 @@ in
   # WARNING: Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+  time.timeZone = "US/Mountain";
 
   nix = {
     package = pkgs.nix;
