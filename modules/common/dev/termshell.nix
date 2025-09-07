c64ee5efa1a23d22ec1202c5031df521a5133b8b
Simon Walker <simon@simonwalker.tech>
2025-09-06 20:48:23 -0600

    add claude-code

--- modules/common/dev/termshell.nix
+++ modules/common/dev/termshell.nix
@@ -2,6 +2,7 @@
   config,
   root,
   pkgs,
+  pkgs-unstable,
   lib,
   ...
 }:
@@ -16,6 +17,7 @@
     pkgs.parallel
     pkgs.mitmproxy
     pkgs.wget
+    pkgs-unstable.claude-code
   ]
   ++ lib.optional config.isLinux pkgs.ghostty;
 
