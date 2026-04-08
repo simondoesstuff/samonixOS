e0fe821b868e43173291668dcb0d452f269c7a4d
Simon Walker <simon@simonwalker.tech>
2026-04-08 05:46:27 -0600

    rm discord

--- modules/common/personal/social.nix
+++ modules/common/personal/social.nix
@@ -12,10 +12,10 @@
       #INFO: On mac, since this modifies signed discord, it now has to be allowed in security settings.
       # ALso, see https://github.com/NixOS/nixpkgs/issues/208749 for errors with openasar "module not found"
       # TLDR: Open regular discord once to get openASAR to work, and then enable openASAR if you desire it
-      (pkgs.discord.override {
-        # withVencord = true;
-        # withOpenASAR = true;
-      })
+      # (pkgs.discord.override {
+      #   # withVencord = true;
+      #   # withOpenASAR = true;
+      # })
       pkgs.slack
 
       # TODO: re-enable once mac signal is back on 25.05
