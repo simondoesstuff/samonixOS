74e00ba094aafed2892eeabea23703d35dc47089
Simon Walker <simon@simonwalker.tech>
2026-08-23 19:49:55 -0600

    feat: git auto sign commits

--- modules/common/dev/git.nix
+++ modules/common/dev/git.nix
@@ -42,6 +42,8 @@
         prune = true; # auto prune deleted remote branches from local
         pruneTags = true; # auto prune deleted remote tags from local
       };
+      # auto sign commits
+      commit.gpgsign = true;
     };
 
     ignores = [
